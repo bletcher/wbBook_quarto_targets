7a5918bcccfc4aaf333e32e52252ddda23f0ac24
Ben Letcher <bletcher@usgs.gov>
2022-11-22 13:32:27 -0500

    add flowByYear

--- R/getDataEnv_targets.R
+++ R/getDataEnv_targets.R
@@ -14,9 +14,31 @@ getDaymet <- function(lat, lon, start_year, end_year) {
   return(out)
 }
 
+# loads 'WBFlow'
+getFlowByRiver <- function(){
+  load('./dataIn/wbFlow/WBFlow_fromJenn.RData')
+  wbFlow2 <- WBFlow %>%
+    select(date, ETmm, Pmm, OB_Flow_cfs, WL_Flow_cfs, MB_Flow_cfs, JB_Flow_cfs) %>%
+    pivot_longer(
+      cols = ends_with("_Flow_cfs"),
+      names_to = c("river", "flow", "cfs"),
+      names_sep = "_",
+      values_to = "flowByRiver") %>%
+    mutate(river = recode(river, "WL" = 'west brook' ,"JB" = 'wb jimmy',"MB" = 'wb mitchell',"OB" = "wb obear"),
+           dateDate = as_date(date),
+           flowByRiverm3s = flowByRiver * 0.028316847) %>%
+    mutate(
+      riverOrdered = factor(river, levels = c('west brook','wb jimmy','wb mitchell',"wb obear"),
+                                   labels = c("West Brook","WB Jimmy","WB Mitchell","WB OBear"), ordered = T)
+    ) %>%
+    select(!c("flow", "cfs", "river", "date")) 
+  return(wbFlow2)
+}
+
 getEnvData_target <-
   tar_plan(
     WB_daymet_target = getDaymet(42.43896889699634, -72.67994313694251, 1997, 2021),
+    flowByRiver = getFlowByRiver(), # data from Jenn's modeling
     
     envDataWB_target = 
       tbl(conDplyr, "data_daily_temperature") %>% 
@@ -31,7 +53,15 @@ getEnvData_target <-
       mutate(riverOrdered = factor(river, levels = c('west brook', 'wb jimmy', 'wb mitchell',"wb obear"),
                                    labels = c("West Brook","WB Jimmy","WB Mitchell","WB OBear"), ordered = T)
       ) %>%
-      left_join(WB_daymet_target, by = c('year', 'yday'))
+      left_join(WB_daymet_target, by = c('year', 'yday')) %>%
+      left_join(flowByRiver)
   )  
 
 ######################################################
+# tmp = envDataWB
+# flowByRiver = getFlowByRiver()
+# tmp2=tmp%>%left_join(flowByRiver)
+# ggplot(tmp2%>%filter(year==2002), aes(dateDate,flowByRiverm3s)) +
+#   geom_point() +
+#   geom_point(aes(dateDate, flow, color = "blue")) +
+#   facet_wrap(~river)
