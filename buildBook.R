965415c671559d4b698bd1a38743c9c20f12f606
Ben Letcher <bletcher@usgs.gov>
2022-12-06 12:36:05 -0500

    book with ft models

--- buildBook.R
+++ buildBook.R
@@ -11,3 +11,4 @@ quarto::quarto_render(output_format = "html")
 
 quarto::quarto_render("dataAll.qmd", output_format = "html")
 quarto::quarto_render("getDataEnv.qmd", output_format = "html")
+quarto::quarto_render("modelsCMR_ft_Flow_OB.qmd", output_format = "html")
