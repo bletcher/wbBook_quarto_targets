0e2e2d792515031afefb734fbaa638307b074faa
Ben Letcher <bletcher@usgs.gov>
2022-12-22 16:15:09 -0500

    add flow/flowbyriver comparison

--- buildBook.R
+++ buildBook.R
@@ -1,9 +1,13 @@
 library(targets)
 library(tarchetypes)
 library(quarto)
+library(knitr)
 
 # run when there is a change in the data
 #tar_invalidate(c("envDataWB_Target", "cdWB_electro0_target", ...)
+
+# to convert markdown headers to quarto in-body chunk options
+convert_chunk_header("dataAll.qmd", output = identity)
 
 tar_make()
 quarto::quarto_render(output_format = "html")
@@ -12,5 +16,4 @@ quarto::quarto_render(output_format = "html")
 quarto::quarto_render("dataAll.qmd", output_format = "html")
 quarto::quarto_render("getDataEnv.qmd", output_format = "html")
 
-quarto::quarto_render("modelsCMR_ft_Flow_OB.qmd", output_format = "html")
 quarto::quarto_render("modelsCMR_ft_cohort_Flow_OB.qmd", output_format = "html")
