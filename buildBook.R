866bbdb4898481e05302f4ed553c07be43c0abfa
Ben Letcher <bletcher@usgs.gov>
2023-01-20 11:41:13 -0500

    update propNegGrowth

--- buildBook.R
+++ buildBook.R
@@ -16,4 +16,4 @@ quarto::quarto_render(output_format = "html")
 quarto::quarto_render("dataAll.qmd", output_format = "html")
 quarto::quarto_render("getDataEnv.qmd", output_format = "html")
 
-quarto::quarto_render("modelsCMR_ft_cohort_Flow_OB.qmd", output_format = "html")
+quarto::quarto_render("modelGrowthInMass.qmd", output_format = "html")
