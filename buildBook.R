0f425a37f8123c49b1dea4dc7c7fd50e67b67a2a
Ben Letcher <bletcher@usgs.gov>
2023-09-22 15:33:15 -0400

    update dataAll

--- buildBook.R
+++ buildBook.R
@@ -24,7 +24,7 @@ tar_make_future(workers = 2)
 quarto::quarto_render(output_format = "html")
 
 
-quarto::quarto_render("modelsCMR_NN_OB.qmd", 
+quarto::quarto_render("dataAll.qmd", 
                       #cache_refresh = TRUE, # default is FALSE
                       output_format = "html")
 
